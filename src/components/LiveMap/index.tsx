72522ff3089ce23725aef10b6d1644922c50e9f7
evanfreymiller <ejf1990@gmail.com>
2022-03-23 21:08:39 -0400

    cleanup

--- src/components/LiveMap/index.tsx
+++ src/components/LiveMap/index.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { useQuery } from "react-query";
-import Map, { Source, Layer } from "react-map-gl";
+import Map from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-import { decode } from "@googlemaps/polyline-codec";
 
 import LiveMarker from "../LiveMarker/index";
-import Pin from "../Pin";
 import { Center, Loader } from "@mantine/core";
-import { VehicleType } from "../VehicleType";
 import { LineShapes } from "../LineShapes/index";
 import { LineDrawer } from "../LineDrawer";
 
-const DAY_MAP = "mapbox://styles/thefreymaster/ckke447ga0wla19k1cqupmrrz";
 const DEFAULT_LATITUDE = 42.35698;
 const DEFAULT_LONGITUDE = -71.06388;
-const DEFAULT_ZOOM = 12;
-
-const MAP_BOX_TOKEN =
-  "pk.eyJ1IjoidGhlZnJleW1hc3RlciIsImEiOiJjbDB2Ymc4cm4xNXZyM2txcGlxandlOTMwIn0.NrtktYJTk0sRUCJdsbhVdg";
 
 export const LiveMap = () => {
   const [isDragging, setIsDragging] = React.useState(false);
@@ -57,7 +49,7 @@ export const LiveMap = () => {
 
   if (isLoading || isLoadingRoutes) {
     return (
-      <Center style={{ width: "100vh", height: "100vw" }}>
+      <Center style={{ height: "100vh", width: "100vw" }}>
         <Loader color="red" size="xl" />
       </Center>
     );
